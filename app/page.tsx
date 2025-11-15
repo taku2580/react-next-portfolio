e416195851a816a893d9cf6e51bfea8e39f66cb5
taku2580 <ktc25a31g0009@edu.kyoto-tech.ac.jp>
2025-11-15 12:36:52 +0900

    3-3-10

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 
+import ButtonLink from "@/app/_components/ButtonLink";
+
 type News = {
   id:string;
   title:string;
@@ -45,7 +47,9 @@ const data: {
 };
 
 export default function Home() {
+  //const sliceData = data.contents.slice(0,2);
   return (
+   <>
     <section className={styles.top}>
       <div>
         <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
@@ -59,5 +63,43 @@ export default function Home() {
         height={1200}
       />
     </section>
+    <section className={styles.news}>
+      <h2 className={styles.newsTitle}>News</h2>
+      <ul>
+        {sliceData.map((article) => (
+          <li key={article.id} className={styles.list}>
+            <div className={styles.link}>
+              <Image
+                className={styles.image}
+                src="/no-image.png"
+                alt="No Image"
+                width={1200}
+                height={630}
+              />
+              <dl className={styles.content}>
+                <dt className={styles.newsItemTitle}>{article.title}</dt>
+                <dd className={styles.meta}>
+                  <span className={styles.tag}>{article.category.name}</span>
+                  <span className={styles.date}>
+                    <Image
+                      src="/clock.svg"
+                      alt=""
+                      width={16}
+                      height={16}
+                      priority
+                    />
+                    {article.publishedAt}
+                  </span>
+                </dd>
+              </dl>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <div className={styles.newsLink}>
+        <ButtonLink href="/news">もっとみる</ButtonLink>
+      </div>
+    </section>
+   </>
   );
 }
